43a8b5450f2fd8f85f09d1a063abfac9af6f6806
FelipeDecome <felipe.decome@hotmail.com>
2019-12-04 19:07:21 -0300

    Update buscar-alunos.php

--- Alura/DoctrineORM/commands/buscar-alunos.php
+++ Alura/DoctrineORM/commands/buscar-alunos.php
@@ -17,8 +17,21 @@ $alunoList = $alunoRepository->findAll();
     echo "ID: {$aluno->getId()} > Nome: {$aluno->getNome()} \n\n";
 } */
 
-$nico = $alunoRepository->find($argv[1]);
-echo $nico->getNome();
+if (isset($argv)) {
+   foreach ($argv as $consults) {
+        if (is_numeric($consults)) {
+            $aluno = $alunoRepository->find($consults);
+            echo $aluno->getNome() . "\n\n";
+        }
+   } 
+}
+
+// foreach ($argv as $consults) {
+//     echo $consults . 'ola';
+// }
+
+//$nico = $alunoRepository->find($argv[1]);
+//echo $nico->getNome();
 
 /* $felipe = $alunoRepository->findOneBy([
     'id' => $argv[1]
