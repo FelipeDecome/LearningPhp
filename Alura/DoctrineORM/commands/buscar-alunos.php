eaf08a93f37ca19dd2814579729a46b6196ab306
FelipeDecome <felipe.decome@hotmail.com>
2019-12-05 19:31:41 -0300

    Doctrine deletar alunos

--- Alura/DoctrineORM/commands/buscar-alunos.php
+++ Alura/DoctrineORM/commands/buscar-alunos.php
@@ -13,18 +13,18 @@ $alunoRepository = $entityManager->getRepository(Aluno::class);
 /** @var Aluno[] $alunoList */
 $alunoList = $alunoRepository->findAll();
 
-/* foreach ($alunoList as $aluno) {
+foreach ($alunoList as $aluno) {
     echo "ID: {$aluno->getId()} > Nome: {$aluno->getNome()} \n\n";
-} */
+}
 
-if (isset($argv)) {
+/* if (isset($argv)) {
    foreach ($argv as $consults) {
         if (is_numeric($consults)) {
             $aluno = $alunoRepository->find($consults);
             echo $aluno->getNome() . "\n\n";
         }
    } 
-}
+} */
 
 // foreach ($argv as $consults) {
 //     echo $consults . 'ola';
