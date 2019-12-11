ad0c09fba07085a43caaaca249df4d5808f68df4
FelipeDecome <felipe.decome@hotmail.com>
2019-12-11 14:23:21 -0300

    whyyy

--- Alura/DoctrineORM/commands/criar-aluno.php
+++ Alura/DoctrineORM/commands/criar-aluno.php
@@ -1,6 +1,7 @@
 <?php
 
 use Felipe\Doctrine\Entity\Aluno;
+use Felipe\Doctrine\Entity\Telefone;
 use Felipe\Doctrine\Helper\EntityManagerFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -8,7 +9,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $aluno = new Aluno();
 $aluno->setNome($argv[1]);
 
-// for ($i = 2);
+for ($i = 2; $i <= $argc; $i++) {
+    $numeroTelefone = $argv[$i];
+    $telefone = new Telefone();
+    $telefone->setNumero($numeroTelefone);
+
+    $aluno->addTelefone($numeroTelefone);
+}
 
 $entityManagerFactory = new EntityManagerFactory();
 $entityManager = $entityManagerFactory->getEntityManager();
--- Alura/DoctrineORM/src/Entity/Telefone.php
+++ Alura/DoctrineORM/src/Entity/Telefone.php
@@ -51,4 +51,3 @@ class Telefone
         return $this;
     }
 }
-
