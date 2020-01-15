6c8e67afa760f3f71141a790d32d48a2e65e3b36
FelipeDecome <felipe.decome@hotmail.com>
2020-01-15 07:43:38 -0300

    doctrine Entity Curso

---
+++ Alura/DoctrineORM/src/Entity/Curso.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Felipe\Doctrine\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * @Entity
+ */
+class Curso
+{
+
+    /**
+     * @Id
+     * GeneratedValue
+     * @Column (type="integer")
+     */
+    private $id;
+
+    /**
+     * Column (type="string")
+     */
+    private $name;
+
+    /**
+     * @ManyToMany (targetEntity="Aluno", inversedBy="cursos")
+     */
+    private $alunos;
+
+    public function __construct()
+    {
+        $this->aluno = new ArrayCollection();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
+    public function setName($name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function addAluno(Aluno $aluno): self
+    {
+
+        if ($this->alunos->contains($aluno)) {
+            return $this;
+        }
+
+        $this->alunos->add($aluno);
+        $aluno->addCurso($this);
+
+        return $this;
+    }
+
+    public function getAlunos(): Collection
+    {
+        return $this->alunos;
+    }
+}
---
+++ Alura/DoctrineORM/src/Migrations/Version20200115101526.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Felipe\Doctrine\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200115101526 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE Aluno (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome VARCHAR(255) NOT NULL COLLATE BINARY)');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE Telefone (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, numero VARCHAR(255) NOT NULL COLLATE BINARY, aluno_id INTEGER DEFAULT NULL)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE Aluno');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE Telefone');
+    }
+}
